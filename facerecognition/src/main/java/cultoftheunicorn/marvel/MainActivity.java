9383683a2b909a899088ae842c61dd5b6cb6f3dc
Manuel Stadler <manuel.sta@gmail.com>
2019-12-12 14:51:16 +0100

    clear facedata button added

--- facerecognition/src/main/java/cultoftheunicorn/marvel/MainActivity.java
+++ facerecognition/src/main/java/cultoftheunicorn/marvel/MainActivity.java
@@ -2,12 +2,17 @@ package cultoftheunicorn.marvel;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import org.opencv.android.OpenCVLoader;
 import org.opencv.cultoftheunicorn.marvel.R;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
 
         Button recognizeButton = (Button) findViewById(R.id.recognizeButton);
         Button trainingButton = (Button) findViewById(R.id.trainingButton);
+        Button clearData = (Button) findViewById(R.id.clearData);
+
 
         recognizeButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,5 +47,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        clearData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String mPath = Environment.getExternalStorageDirectory() + "/facerecogOCV/";
+                deleteDirectory(new File(mPath));
+                Toast.makeText(MainActivity.this, "Facedata cleared!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        OpenCVLoader.initDebug();
+        System.loadLibrary("opencv_java");
+    }
+
+    public static boolean deleteDirectory(File path) {
+        if( path.exists() ) {
+            File[] files = path.listFiles();
+            if (files == null) {
+                return true;
+            }
+            for(int i=0; i<files.length; i++) {
+                if(files[i].isDirectory()) {
+                    deleteDirectory(files[i]);
+                }
+                else {
+                    files[i].delete();
+                }
+            }
+        }
+        return( path.delete() );
     }
 }
